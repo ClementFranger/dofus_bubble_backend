84ef6e875b87d86f30bf477660b401967d3ab619
Minifranger <clement.franger@gmail.com>
2020-07-04 10:41:11 +0200

    added check body decorator

--- dofus_bubble/dynamodb/put_item.py
+++ dofus_bubble/dynamodb/put_item.py
@@ -2,21 +2,14 @@ import json
 import logging
 
 from dofus_bubble.dynamodb.dynamodb import DynamoDB
-from utils import failure, load_body
+from utils import failure, load_body, check_body_id
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
 @load_body
+@check_body_id('_id')
 def put_item(event, context, **kwargs):
     logger.info('event : {event}'.format(event=event))
-
-    # try:
-    #     body = json.loads(event.get('body'))
-    # except TypeError as e:
-    #     return failure(body='Error when parsing body : {e}'.format(e=e))
-    if not kwargs.get('body').get('_id'):
-        return failure(code=400, body='You should provide a _id key to your body')
-
-    return DynamoDB().put(Item=event.get('body'))
+    return DynamoDB().put(Item=kwargs.get('body'))
