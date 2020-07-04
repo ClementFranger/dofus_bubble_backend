4c09ed01bc394b5f475b7708aa864444bf52f684
Minifranger <clement.franger@gmail.com>
2020-07-04 10:18:25 +0200

    new layer

--- dofus_bubble/dynamodb/put_item.py
+++ dofus_bubble/dynamodb/put_item.py
@@ -2,18 +2,21 @@ import json
 import logging
 
 from dofus_bubble.dynamodb.dynamodb import DynamoDB
-from utils import failure
+from utils import failure, load_body
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-def put_item(event, context):
+@load_body
+def put_item(event, context, **kwargs):
     logger.info('event : {event}'.format(event=event))
 
-    body, id = event.get('body'), event.get('body').get('id')
-    # body = json.loads(body) if isinstance(body, str) else body
-    if not body and not id:
+    # try:
+    #     body = json.loads(event.get('body'))
+    # except TypeError as e:
+    #     return failure(body='Error when parsing body : {e}'.format(e=e))
+    if not kwargs.get('body').get('_id'):
         return failure(code=400, body='You should provide a _id key to your body')
 
-    return DynamoDB().put(Item=body)
+    return DynamoDB().put(Item=event.get('body'))
