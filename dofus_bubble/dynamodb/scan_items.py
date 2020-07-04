c6b08080d05f2b930ddb52b9c444299838b8afc1
Minifranger <clement.franger@gmail.com>
2020-07-04 20:28:34 +0200

    renamed func and nex layer

--- dofus_bubble/dynamodb/scan_items.py
+++ dofus_bubble/dynamodb/scan_items.py
@@ -2,5 +2,5 @@ from dynamodb.dynamodb import DynamoDB
 from utils import success
 
 
-def get_all_items(event, context, **kwargs):
+def scan_items(event, context, **kwargs):
     return success(body=DynamoDB(**kwargs).scan().get('Items'))
