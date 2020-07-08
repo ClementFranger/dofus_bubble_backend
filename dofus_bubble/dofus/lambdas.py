948f4c918862052ef073e30f01f9c20449bda07f
Minifranger <clement.franger@gmail.com>
2020-07-08 14:39:24 +0200

    changed id to number, so fix mock and raise error

--- dofus_bubble/dofus/lambdas.py
+++ dofus_bubble/dofus/lambdas.py
@@ -7,9 +7,16 @@ class LambdasDofus(Lambdas):
 
     @staticmethod
     def scan_weapons_by_price(*args, **kwargs):
-        weapons = scan_weapons(*args, **kwargs)
-        items = scan_items(*args, **kwargs)
-        return {'statusCode': 200, 'body': [weapons, items]}
+        weapons = scan_weapons(*args, **kwargs).get('body')
+        items = scan_items(*args, **kwargs).get('body').get('Items')
+        result = [{**w, **i} for i in items for w in weapons if w.get('_id') == i.get('_id')]
+        # print(items)
+        # print(weapons[0])
+        # for w in weapons:
+        #     if w.get('_id') == 44:
+        #         print(w)
+        print(result)
+        return {'statusCode': 200, 'body': result}
 
 
 scan_weapons_by_price = LambdasDofus().scan_weapons_by_price
